c2d59401accc82505f83b415a40388aad14c3026
mwcaq480 <mwcaq480@CUNRZR90G8U32.int.marrcorp.marriott.com>
2018-10-11 10:50:59 -0500

    09/25/2018 - created project for the assigment cityofaaron

---
+++ cityOfAaron/src/model/Location.java
@@ -0,0 +1,15 @@
+/*
+ * The Location class file for the cityOfAaron project
+ * CIT-260
+ * Spring 2018
+ * Author: Wladimir Canar
+ */
+package model;
+
+/**
+ *
+ * @author mwcaq480
+ */
+public class Location {
+    
+}
