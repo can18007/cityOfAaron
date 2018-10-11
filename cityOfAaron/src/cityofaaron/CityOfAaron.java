bbf52e5943576547bee371289eb83952bac7987c
mwcaq480 <mwcaq480@CUNRZR90G8U32.int.marrcorp.marriott.com>
2018-10-11 10:05:56 -0500

    09/25/2018 - created project for the assigment cityofaaron

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * test changes wcanar 
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Spring 2018
+ * Team members: Megan Hendrickson, McKell Painter, Wladimir Canar
  */
 package cityofaaron;
 
