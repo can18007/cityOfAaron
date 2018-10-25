b68f99f7d1572d19e039a8318a490b12dfd180a5
Megan <Megan@Megan-PC>
2018-10-25 11:11:52 -0500

    10/25 Created control layer and CropControl class. Started working on sellLand method. Added method prologue and psuedo code. ~Megan

---
+++ cityOfAaron/src/control/CropControl.java
@@ -0,0 +1,35 @@
+/*
+ * The CropControl class - part of the control layer for the City of Aaron Project
+ * class contains all of the calculation methods for managing the crops
+ * CIT-260
+ * Spring 2018
+ * Team members: Megan Hendrickson, McKell Painter, Wladimir Canar
+ */
+
+package control;
+
+public class CropControl 
+{
+/* The sellLand method
+ * Purpose: To sell land
+ * Parameters: the price of land, the number of acres to sell, and 
+ *  a reference to a cropData object
+ * Retruns: the nubmer of acres owned after the sale
+ * Pre-conditions: acres to sell must be positive
+ * and<= acresOwned
+*/
+    public static int sellLand(int landPrice, int acresToSell, CropData cropData)
+    {
+        // if acresToSell < 0, return -1
+        
+        // if acresToSell > acresOwned, return -1
+
+        // acresOwned = acresOwned - acresToSell
+        
+        // wheatInStore = wheatInStore + acresToSell * landPrice
+
+        // return acresOwned
+
+
+    }
+}
