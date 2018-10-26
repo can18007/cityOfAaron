64b168bf0b595db6e142721a856d0d342fc8265d
mpainter <HP@10.10.5.32>
2018-10-25 20:58:36 -0600

    10/13 buyLand tests

---
+++ cityOfAaron/test/control/CropControlTest.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import model.CropData;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author HP
+ */
+public class CropControlTest {
+    
+    public CropControlTest() {
+    }
+
+    /**
+     * Test of calcLandCost method, of class CropControl.
+     */
+    @Test
+    public void testCalcLandCost() {
+        System.out.println("calcLandCost");
+        int expResult = 0;
+        int result = CropControl.calcLandCost();
+        assertEquals(expResult, result);;
+    }
+
+    /**
+     * Test of sellLand method, of class CropControl.
+     */
+    @Test
+    public void testSellLand() {
+        System.out.println("sellLand");
+        int landPrice = 0;
+        int acresToSell = 0;
+        CropData cropData = null;
+        int expResult = 0;
+        int result = CropControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand() {
+        System.out.println("buyLand");
+        int landPrice = 0;
+        int acresToBuy = 0;
+        CropData cropData = null;
+        int expResult = 0;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand1() {
+        System.out.println("buyLand");
+        int landPrice = 20;
+        int acresToBuy = 10;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(600);
+        cropData.setWheatInStore(600);
+        int expResult = 610;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand2() {
+        System.out.println("buyLand");
+        int landPrice = 50;
+        int acresToBuy = -10;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(600);
+        cropData.setWheatInStore(600);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand3() {
+        System.out.println("buyLand");
+        int landPrice = 60;
+        int acresToBuy = 10;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(600);
+        cropData.setWheatInStore(60);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand4() {
+        System.out.println("buyLand");
+        int landPrice = 50;
+        int acresToBuy = 10;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(600);
+        cropData.setWheatInStore(600);
+        int expResult = 610;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+        /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand5() {
+        System.out.println("buyLand");
+        int landPrice = 50;
+        int acresToBuy = 0;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(600);
+        cropData.setWheatInStore(600);
+        int expResult = 600;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+}
+
