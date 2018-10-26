c5f23c9199fa0826c6a61bac6698d7ad7a0363ac
mpainter <HP@10.10.5.32>
2018-10-25 21:05:07 -0600

    10/13 test changes

--- cityOfAaron/test/control/CropControlTest.java
+++ cityOfAaron/test/control/CropControlTest.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The Crop Control Test 
+ * CIT-260
+ * Fall 2018
+ * Megan Hendrickson, Wladimir Canar, McKell Painter
  */
 package control;
 
