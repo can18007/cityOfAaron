8997af3b165db3ef7e6c410738ef3fdbcec27cf8
mpainter <HP@10.10.5.32>
2018-11-01 19:56:19 -0600

    11/1 view class

---
+++ cityOfAaron/src/view/StartProgramView.java
@@ -0,0 +1,27 @@
+/*
+ * The StartProgram View class for the City of Aaron project.
+ * CIT-260
+ * Fall 2018
+ * Team Members: Megan Hendrickson, Wladimir Canar, McKell Painter
+ */
+package view;
+
+/**
+ *
+ * @author HP
+ */
+public class StartProgramView {
+    
+    public StartProgramView() {
+//        displayStartProgramView(){
+//        endOfView = false
+//        DO
+//            inputs = getInputs()
+//            IF no inputs were entered OR the first input is Q
+//                RETURN
+//            ENDIF
+//            endOfView = doActio (inputs)
+//        WHILE endOfView != true
+//        }
+    }
+}
