b7c3ce927bde93ec36519e5e2fb0dd994441a271
lgn-cunrz-hr <lgn-cunrz-hr@CUNRZR90G8U32.int.marrcorp.marriott.com>
2018-11-05 10:36:51 -0500

    changes by wcanar

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -15,58 +15,28 @@ import view.*;
  */
 public class CityOfAaron {
 
-    /**
-     * @param args the command line arguments
-     */
     //variable for keeping a reference to the Game object
     private static Game theGame = null;
     
     //main function - etnry point for the program
     //runs the main menu
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {   
         MainMenuView mmv = new MainMenuView();
         mmv.displayMenuView();
-        
-        // TODO code application logic here
-        Player playerOne = new Player();
-        playerOne.setName("Megan");
-        
-        Game game = new Game();
-        game.setPlayer(playerOne);
-
-        Player result = game.getPlayer();
-        System.out.println(result.getName());
-
-        System.out.println(TeamMember.MEGAN.getName() + " - " + TeamMember.MEGAN.getTitle());
-        
-        //Test Location class author wladimir
-        Location myLocation = new Location();
-        myLocation.setDescription("Test Location");
-        myLocation.setSymbol("example symbol");
-        
-        System.out.println(myLocation.getDescription());
-        System.out.println(myLocation.getSymbol());
-        //end test
-        
-        //Test ListItem class author mpainter
-        ListItem aListItem = new ListItem();
-        aListItem.setName("Item1");
-        aListItem.setNumber(1);
-        
-        System.out.println(aListItem.getName());
-        System.out.println(aListItem.getNumber());
-        //end test
-        
-
-        //Test CropData class author Megan Hendrickson
-        CropData myCropData = new CropData();
-        myCropData.setYear(2010);
-        myCropData.setPopulation(27);
-
-        System.out.println(myCropData.getYear());
-        System.out.println(myCropData.getPopulation());
-        
-    }
+    }    
     
+    /**
+     *
+     * @param _theGame
+     */
+    public static void setGame(Game _theGame){
+        CityOfAaron.theGame = _theGame;
+    }    
+
+    public static Game getGame(){
+        return CityOfAaron.theGame;
+        
+ 
+    }
+
 }
