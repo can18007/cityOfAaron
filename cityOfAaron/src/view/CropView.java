c3a09eb7f37db4e44bb1bd7eb4439f7cc3bb8c88
Megan <Megan@Megan-PC>
2018-11-07 22:59:05 -0600

    11/7 - GameMenuView

--- cityOfAaron/src/view/CropView.java
+++ cityOfAaron/src/view/CropView.java
@@ -54,14 +54,35 @@ public class CropView {
     {
         // call the buyLandView( ) method
         buyLandView( );
-
+        
+        // call the sellLandView( ) method
+        sellLandView( );
+        
+        // call the plantCropsVew
+        plantCropsView();
+        
         // add calls to the other crop view methods
         // as they are written
     }
     
      public static void sellLandView()
     {
-       
+        // Generate a random number for the price of an acre of land. Make this value between 17 and 27 bushels per acre
+        int price = CropControl.calcLandCost();
+
+        // Prompt the user to enter the number of acres to sell
+        System.out.format("Land is selling for %d bushels per acre. %n", price);
+        System.out.print("\nHow many acres of land do you wish to sell?");
+
+        //  Get the user’s input and save it.
+        int toSell;
+        toSell = keyboard.nextInt();
+
+        // Call the buyLand( ) method in the control layer to buy the land
+        CropControl.sellLand(price, toSell, cropData);
+
+        //output how much land we now own
+        System.out.format("You now own %d acres of land", cropData.getAcresOwned());  
     }
      
      public static void feedPeopleView()
@@ -71,7 +92,21 @@ public class CropView {
     
     public static void plantCropsView()
     {
-       
+       //Ask the user “How many acres of land do you want to plant”
+       //User enters a value.
+       System.out.format("\nHow many of acres of land do you want to plant?");
+
+        //  Get the user’s input and save it.
+        int toPlant;
+        toPlant = keyboard.nextInt();
+        
+        //call plantCrops() method in the Control Layer 
+        CropControl.plantCrops(toPlant, cropData);
+        
+        //Display the number of acres of wheat that have been planted and the amount of wheat left in storage.
+        System.out.format("You have been panted %d of acres of wheat", cropData.getAcresPlanted());
+        System.out.format("\nYou now have %d wheat left in storage", cropData.getWheatInStore());
+      
     }
     
     public static void displayCropsReportVew()
